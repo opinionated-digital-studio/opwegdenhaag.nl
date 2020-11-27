021bae42b299ab75d41184ecc6156e908eaa1314
pandu-supriyono <pandu@supriyono.me>
2020-11-28 00:38:44 +0100

    feat(components): base button added

---
+++ src/components/OwButton.vue
@@ -0,0 +1,74 @@
+<template>
+  <a href="" class="ow-button" :class="'ow-button' + buttonColorModifier"
+    ><slot
+  /></a>
+</template>
+
+<script>
+export default {
+  name: "OwButton",
+  props: {
+    color: {
+      type: String,
+      required: false,
+      default: "primary",
+    },
+  },
+  computed: {
+    buttonColorModifier: function () {
+      let buttonColorModifierClass;
+      switch (this.color) {
+        case "primary":
+          buttonColorModifierClass = "--primary";
+          break;
+        case "white":
+          buttonColorModifierClass = "--white";
+          break;
+      }
+      return buttonColorModifierClass;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.ow-button {
+  text-decoration: none;
+  padding: 0.4rem 1.2rem;
+  background-color: $primary-color;
+  color: $white;
+  border-radius: $global-radius;
+  position: relative;
+  overflow: hidden;
+  display: inline-block;
+  vertical-align: middle;
+  height: 100%;
+
+  &:before {
+    content: "Maatje worden";
+    position: absolute;
+    padding: 0.4rem 1.2rem;
+    background-color: $secondary-color;
+    color: $primary-color;
+    left: 0;
+    top: 0;
+    clip-path: circle(0% at 0% 0%);
+    transition: all 800ms ease;
+  }
+
+  &:after {
+    background: none;
+  }
+
+  &:hover {
+    &:before {
+      clip-path: circle(250% at 0% 0%);
+    }
+  }
+
+  &--white {
+    background-color: $white;
+    color: $primary-color;
+  }
+}
+</style>

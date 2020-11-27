4f956ee37c0d04d823ffd12be698efcd2a83afcb
pandu-supriyono <pandu@supriyono.me>
2020-11-28 00:39:55 +0100

    feat(components): base jumbotron added

---
+++ src/components/OwJumbotron.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="ow-jumbotron">
+    <div class="ow-container">
+      <div class="ow-jumbotron__body">
+        <h1 class="ow-jumbotron__title">Lorem ipsum dolor sit amet</h1>
+        <p class="ow-type__p ow-type__p--lead">
+          De maatjes van Op weg begeleiden Hagenaars die te maken hebben gehad
+          met een psychose
+        </p>
+        <a class="ow-jumbotron__link" href=""
+          >Maak kennis met ons werk &#8595;</a
+        >
+      </div>
+    </div>
+    <svg class="ow-jumbotron__circle ow-jumbotron__circle--left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500">
+      <g data-name="Layer 2">
+        <circle cx="250" cy="250" r="250" data-name="Layer 1" />
+      </g>
+    </svg>
+    <svg class="ow-jumbotron__circle ow-jumbotron__circle--right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500">
+      <g data-name="Layer 2">
+        <circle cx="250" cy="250" r="250" data-name="Layer 1" />
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OwJumbotron",
+};
+</script>
+
+<style lang="scss" scoped>
+.ow-jumbotron {
+  background-color: $primary-color;
+  color: $white;
+  padding: 6rem 0 14rem 0;
+  position: relative;
+  overflow: hidden;
+
+  &__title {
+    margin: 0;
+
+    @include fullhd() {
+      font-size: 3.815rem;
+      margin-bottom: 0.5rem;
+    }
+  }
+
+  &__link {
+    color: $white;
+    font-weight: 400;
+
+    &:after {
+      background-color: $secondary-color;
+    }
+  }
+
+  &__body {
+    @include desktop() {
+      padding-right: 40%;
+    }
+  }
+  
+  &__circle {
+    position: absolute;
+    fill: $secondary-color;
+
+    &--left {
+      width: 24rem;
+      left: -10rem;
+      bottom: -12rem;
+    }
+
+    &--right {
+      width: 30rem;
+      right: -20rem;
+      top: -15rem;
+    }
+  }
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,43 +1,23 @@
 <template>
-  <Layout>
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image
-      alt="Example image"
-      src="~/favicon.png"
-      width="135"
-    />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a
-        href="https://gridsome.org/docs/"
-        target="_blank"
-        rel="noopener"
-      >Gridsome Docs</a>
-      <a
-        href="https://github.com/gridsome/gridsome"
-        target="_blank"
-        rel="noopener"
-      >GitHub</a>
-    </p>
+  <Layout :header-style="headerStyle" >
+    <ow-jumbotron />
   </Layout>
 </template>
 
 <script>
+import OwJumbotron from "~/components/OwJumbotron.vue";
 export default {
   metaInfo: {
-    title: 'Hello, world!',
+    title: "Startpagina",
   },
+  components: { OwJumbotron },
+  data() {
+    return {
+      headerStyle: 'primary'
+    }
+  }
 };
 </script>
 
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
+<style lang="scss" scoped>
 </style>

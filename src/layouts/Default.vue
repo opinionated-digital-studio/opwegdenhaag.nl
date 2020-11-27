25e0df06dceb939b1d12e0b04c92ca7fe31b6645
pandu-supriyono <pandu@supriyono.me>
2020-11-28 00:38:27 +0100

    fix(styles): added normalizer to css and various base style fixes

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="layout">
-    <TheHeader />
+    <the-header :header-style="headerStyle" />
     <slot />
   </div>
 </template>
@@ -13,7 +13,24 @@ query {
 }
 </static-query>
 
+<script>
+import TheHeader from "~/components/TheHeader";
+export default {
+  components: { TheHeader },
+  props: {
+    headerStyle: {
+      type: String, required: false, default: 'white'
+    }
+  }
+};
+</script>
+
 <style lang="scss">
+.ow-container {
+  max-width: 2160px;
+  margin: 0 auto;
+  padding: 0 6%;
+}
 html {
   font-size: 112.5%;
 } /*18px*/
@@ -69,59 +86,33 @@ small,
 
 @include desktop() {
   html {
-    font-size: 125%;
-  } /*20px*/
+    font-size: 137.5%;
+  } /*22px*/
+}
 
-  body {
-    background: white;
-    line-height: 1.6;
-    color: #000000;
+a {
+  text-decoration: none;
+  display: inline-block;
+  position: relative;
+
+  &:after {
+    content: '';
+    position: absolute;
+    background-color: $black;
+    height: 1px;
+    width: 100%;
+    bottom: 0;
+    left: 0;
   }
+}
 
-  p {
-    margin-bottom: 1rem;
-  }
-
-  h1,
-  h2,
-  h3,
-  h4,
-  h5 {
-    margin: 3rem 0 1.38rem;
-    line-height: 1.3;
-  }
-
-  h1 {
-    margin-top: 0;
-    font-size: 3.052rem;
-  }
-
-  h2 {
-    font-size: 2.441rem;
-  }
-
-  h3 {
-    font-size: 1.953rem;
-  }
-
-  h4 {
-    font-size: 1.563rem;
-  }
-
-  h5 {
-    font-size: 1.25rem;
-  }
-
-  small,
-  .text_small {
-    font-size: 0.8rem;
+.ow-type {
+  &__p {
+    &--lead {
+      @extend h4;
+      font-weight: 700;
+      margin-top: 0;
+    }
   }
 }
 </style>
-
-<script>
-import TheHeader from "~/components/TheHeader";
-export default {
-  components: { TheHeader },
-};
-</script>

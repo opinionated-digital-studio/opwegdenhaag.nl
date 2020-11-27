38f3ced10030407d35877e88b361715ba09d0167
pandu-supriyono <pandu@supriyono.me>
2020-11-28 00:36:55 +0100

    eslint now conforming es6

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
   parser: "vue-eslint-parser",
+  parserOptions: {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+  },
   plugins: ["gridsome"],
   rules: {
     "gridsome/format-query-block": "error"

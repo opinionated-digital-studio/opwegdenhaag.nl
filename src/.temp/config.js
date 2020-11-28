ae53656415da65ceb1c9427a748dd1a2b939ddb7
pandu-supriyono <pandu@supriyono.me>
2020-11-28 01:53:16 +0100

    rm temp

--- src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - Op weg Den Haag",
-  "siteUrl": "",
-  "version": "0.7.22",
-  "catchLinks": true
-}
--- src/.temp/now.js
+++
@@ -1,1 +0,0 @@
-export default 1606520977251

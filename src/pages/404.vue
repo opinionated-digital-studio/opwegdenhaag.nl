3abfdb92de0c008cdbf98c6cb8172f08570b8649
pandu-supriyono <mail@supriyono.nl>
2021-01-18 14:13:39 +0100

    feat: 404 page added

---
+++ src/pages/404.vue
@@ -0,0 +1,27 @@
+<template>
+  <Layout>
+    <div class="columns">
+      <div class="column is-one-third">
+        <g-link to="/">&#8592; Terug naar startpagina</g-link>
+      </div>
+      <div class="column">
+        <h1 class="page__title">Pagina niet gevonden</h1>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column is-offset-one-third ow-page-body">De pagina die je zoekt kan niet worden gevonden. Dit kan omdat de pagina niet (meer) bestaat of omdat er een
+        typefout is gemaakt bij het invoeren van het adres.
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+    title: 'Sorry, deze pagina is niet beschikbaar',
+    }
+  }
+};
+</script>
